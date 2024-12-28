8f5b7726a6c8845e787a147c9a30d07cd9776805
Suresh Roshi <sureshroshi007@gmail.com>
2024-12-28 17:24:50 +0530

    modified vpc naming conventions

--- vpc/deploy.sh
+++ vpc/deploy.sh
@@ -4,7 +4,7 @@ STACK_NAME=FsocietyVPCStack
 
 aws cloudformation create-stack \
     --stack-name $STACK_NAME \
-    --template-body file://vpc-template.yml \
+    --template-body file://template.yml \
     --parameters ParameterKey=VpcCidr,ParameterValue=172.16.0.0/16 \
     ParameterKey=PublicSubnetCidr,ParameterValue=172.16.1.0/24 \
     ParameterKey=PrivateSubnetCidr,ParameterValue=172.16.2.0/24
